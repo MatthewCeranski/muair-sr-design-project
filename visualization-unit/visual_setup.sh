9de61616ab04a78ec6ae81da2981cc653a3bc424
MatthewCeranski <161319775+MatthewCeranski@users.noreply.github.com>
2025-05-01 11:18:43 -0500

    Update visual_setup.sh

--- visualization-unit/visual_setup.sh
+++ visualization-unit/visual_setup.sh
@@ -1,1 +1,7 @@
-
+this file needs to:
+  install python on the raspberry pi
+  install relevant libraries for the function of MUAIR
+  organize folders in the same manner as the original raspberry pis
+  copy python files into correct folders
+  copy .sh files into correct folders
+  set up crontasks for each .sh file in the same manner as the original raspberry pi
